54a6d6b6b658e1c3ec1a8bd17075999c2641a0e9
Celina Alcantara <celinaacarmo@gmail.com>
2022-03-18 17:03:48 -0300

    Aplicativo de dados Duas telas Dia 18/03

---
+++ app/src/main/java/com/example/appdedados/Activity2.kt
@@ -0,0 +1,20 @@
+package com.example.appdedados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Activity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+
+        val buttonVoltar = findViewById<Button>(R.id.buttonVoltar)
+        buttonVoltar.setOnClickListener {
+            finish()
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/appdedados/MainActivity.kt
+++ app/src/main/java/com/example/appdedados/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.appdedados
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +13,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonD6 = findViewById<Button>(R.id.buttonD6)
-        buttonD6.setOnClickListener {
-            rolarDados(6)
-        }
-
         val buttonD12 = findViewById<Button>(R.id.buttonD12)
-        buttonD12.setOnClickListener {
-            rolarDados(12)
+        val buttonD20 = findViewById<Button>(R.id.buttonD20)
+
+        val intentDados = Intent(this, Activity2::class.java)
+
+        buttonD6.setOnClickListener {
+            intentDados.putExtra("lados", 6)
+            startActivity(intentDados)
         }
 
-        val buttonD20 = findViewById<Button>(R.id.buttonD20)
-        buttonD20.setOnClickListener {
-            rolarDados(20)
+        buttonD12.setOnClickListener {
+            intentDados.putExtra("lados", 12)
+            startActivity(intentDados)
         }
+
+        buttonD20.setOnClickListener {
+            intentDados.putExtra("lados", 20)
+            startActivity(intentDados)
+        }
+
+        Log.d("CicloDeVida", "onCreate()")
 
     }
 
@@ -38,22 +47,45 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    /*val imageResourse = imagemD6(valor)
+    override fun onStart() {
+        super.onStart()
+        Log.d("CicloDeVida","onStart()")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("CicloDeVida","onResume()")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d("CicloDeVida","onPause()")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("CicloDeVida","onStop()")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("CicloDeVida","onRestart()")
+    }
 
-        val imageDado = findViewById<ImageView>(R.id.imageDado)
-
-        imageDado.setImageResource(imageResourse)*/
 
 
-  /*  private fun imagemD6(valor: Int): Int {
-        return when(valor){
-            1 -> R.drawable.dado_1
-            2 -> R.drawable.dado_2
-            3 -> R.drawable.dado_3
-            4 -> R.drawable.dado_4
-            5 -> R.drawable.dado_5
-            else -> R.drawable.dado_6
-        }
-    }*/
+        /*val imageResourse = imagemD20(valor)
+
+        val imgDado = findViewById<ImageView>(R.id.imgDado)
+
+        imgDado.setImageResource(imageResourse)
+
+
+        private fun imagemD20(valor: Int): Int {
+            return when (valor) {
+                1 -> R.drawable.dado_1
+                2 -> R.drawable.dado_2
+                3 -> R.drawable.dado_3
+                4 -> R.drawable.dado_4
+                5 -> R.drawable.dado_5
+                else -> R.drawable.dado_6
+            }
+        }*/
 
 }
